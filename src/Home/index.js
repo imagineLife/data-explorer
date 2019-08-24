b617548f08743043546a79f306d3f51a31ee129b
Eric Laursen <ericjlaursen@gmail.com>
2019-08-24 15:27:21 -0400

    conditionally renders no file data

--- src/Home/index.js
+++ src/Home/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 const Home = () => {
-	return(
-	<div>
-		<h2>Home</h2>
-	</div>
-	)
+	let [fileData, setFiledata] = React.useState(null)
+
+	
+	if(!fileData){
+	  return(<p>No File Data</p>)
+	}
+			
 }
 
 export default Home
