51fbfb14667c7d3f487a1eb42e35d72870df017a
Eric Laursen <ericjlaursen@gmail.com>
2019-08-25 19:59:13 -0400

    includes svg with "responsive" width

--- src/BarChart/index.js
+++ src/BarChart/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import { makeScaleType } from '../helpers' 
-import useDims from '../Hooks/useDims'
+import useDimensions from '../Hooks/useDimensions'
 
 const BarChart = ({axis, fileData}) => {
-	const dims = useDims()
-	console.log('dims')
-	console.log(dims)
+	let thisRef = React.useRef()
+	const [ref, {width}] = useDimensions();
+	console.log('width')
+	console.log(width)
 	
 	
-	let {x, y} = axis
-	let { xType, xVal } = x;
-	let { yType, yVal } = y;
+	let {x: xAxis, y: yAxis} = axis
+	let { xType, xVal } = xAxis;
+	let { yType, yVal } = yAxis;
 
 	let xScale = makeScaleType(xType, fileData, xVal)
 	let yScale = makeScaleType(yType, fileData, yVal)
 			
 	return (
-	  <div id="chartDiv" style={{height: '500px'}}>
+	  <div id="chartDiv" style={{height: '500px'}} ref={ref}>
+	  	<svg width={width} height="100%" style={{border: "1px solid orange"}} />
 	  </div>
 	)
 }
