fb9605255c25b5a475acdb853a0b451f6d8699ff
Eric Laursen <ericjlaursen@gmail.com>
2019-08-27 06:00:20 -0400

    renders bar chart

--- src/BarChart/index.js
+++ src/BarChart/index.js
@@ -1,41 +1,62 @@
 import React from 'react';
 import { makeScaleType } from '../helpers' 
 import useDimensions from '../Hooks/useDimensions'
+import AxesAndMath from '../Components/AxesAndMath'
 
 const BarChart = ({axis, data, w, h}) => {
 	let thisRef = React.useRef()
 	const [ref, {width}] = useDimensions();
-	let [margins] = React.useState({t:10, r: 10, b: 10, l: 10})	
+	let [margins] = React.useState({t:20, r: 10, b: 75, l: 40})	
 
 	//destructure props	
 	let {x: xAxis, y: yAxis} = axis
 	let { type: xType, key: xVal } = xAxis;
 	let { type: yType, key: yVal } = yAxis;
-	
-	//set dimensions-less-margins
-	let wLM = width - margins.l - margins.r
-	let hLM = width - margins.t - margins.b
 
 	//update scales when width is changed
-	if(width && h && xVal && yVal){
-	  let xScale = makeScaleType(xType, data, xVal)
-	  xScale.range([margins.l, width - margins.r]);
+	if(width && width && h && xVal && yVal){
 
+	  //set dimensions-less-margins
+	  let wLM = width - margins.l - margins.r
+	  let hLM = h - margins.t - margins.b
+
+	  let xScale = makeScaleType(xType, data, xVal)
+	  xScale.range([0, wLM]);
+	  console.log('xScale.domain()')
+	  console.log(xScale.domain())
+	  
 	  let yScale = makeScaleType(yType, data, yVal);
-	  yScale.range([h - margins.b, margins.t]);
+	  yScale.range([hLM - margins.b, margins.t]);
 	  
 	  let rects = data.map((d, ind) => {
 	  	return <rect
 	  		key={`${ind}${d[xVal]}`}
-	  		x={xScale(d[xVal])}
-	  		y={yScale(d[yVal])}
-	  		height={h - margins.b - yScale(d[yVal])}
+	  		x={xScale(d.x)}
+	  		y={yScale(d.y)}
+	  		height={hLM - yScale(d.y)}
+	  		fill={'steelblue'}
 	  		width={xScale.bandwidth()}></rect>
 	  })
+
+	  let svgDimensions = {
+	  	height: h - margins.t,
+	  	width: width - margins.l
+	  }
+
+	  console.log('svgDimensions')
+	  console.log(svgDimensions)
+	  
 	  return (
 	  <div id="chartDiv" style={{height: h, width: w}} ref={ref}>
 	  	<svg className="barChart" width={width} height="100%" style={{border: "1px solid orange"}}>
 	  	  <g transform={`translate(${margins.l}, ${margins.t})`}>
+	  	  	
+	  	  	<AxesAndMath
+	          scales={{ xScale, yScale }}
+	          margins={margins}
+	          svgDimensions={svgDimensions}
+	        />
+
 	  	  	{rects}
 	  	  </g>
 	  	</svg>
