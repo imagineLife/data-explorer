9c836da8598646c6d6c9dbf594faf8fa71a1b106
Eric Laursen <ericjlaursen@gmail.com>
2019-08-28 14:02:02 -0400

    updates import/export from dataPrep

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,4 +1,4 @@
 import makeScaleType from './makeScales'
 import wrap from './wrap'
-import { prepCountByIncomeData } from './dataPrep'
-export { makeScaleType, wrap, prepCountByIncomeData }
+import { prepCountByIncomeData, prepCountByYearsData } from './dataPrep'
+export { makeScaleType, wrap, prepCountByIncomeData, prepCountByYearsData }
