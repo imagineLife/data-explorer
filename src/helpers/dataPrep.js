95fd55fcf9167c139d69f23f06d045bf81dce959
Eric Laursen <ericjlaursen@gmail.com>
2019-08-28 14:02:23 -0400

    new dataPrep fn

--- src/helpers/dataPrep.js
+++ src/helpers/dataPrep.js
@@ -27,7 +27,45 @@ const prepCountByIncomeData = srcData => {
 		  resArr = newResArr
 		}
 	})
+	
 	return resArr
 }
 
-export { prepCountByIncomeData }
+const prepCountByYearsData = srcData => {
+	let resArr = []
+	/*
+		PREP BAR DATA
+		x = 
+	*/
+	srcData.forEach(d => {
+		let thisXInResArr = resArr.filter(ra => ra.x == d["q42"])
+		
+		if(thisXInResArr.length < 1){
+			resArr.push({
+			  x: d["q42"],
+			  y: 1
+			})
+		}else{
+		  thisXInResArr = thisXInResArr[0]
+		  thisXInResArr.y = thisXInResArr.y + 1
+		  
+		  let newResArr = resArr.map(ra => {
+		  	if(ra.x == thisXInResArr.x){
+		  		return thisXInResArr
+		  	}else{
+		  		return ra
+		  	}
+		  })
+		
+		  resArr = newResArr
+		}
+	})
+
+	resArr.sort((a,b) => a.x - b.x)
+	console.log('resArr')
+	console.log(resArr)
+
+	return resArr
+}
+
+export { prepCountByIncomeData, prepCountByYearsData }
