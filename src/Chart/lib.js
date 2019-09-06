8499ab74900ae6010d96d514b089fa411b2e188d
Eric Laursen <ericjlaursen@gmail.com>
2019-09-06 05:58:51 -0400

    separates line && circle from lollipop

--- src/Chart/lib.js
+++ src/Chart/lib.js
@@ -1,19 +1,29 @@
 import React from 'react';
 
-function makeLollipops(d,ind,xVal,yScale,xScale){
-	return(<g key={`${ind}${d[xVal]}`}>
-		<line
+function makeLine(xScale, yScale, d){
+	return <line
   		x1={xScale(d.x)}
 		  x2={xScale(d.x)}
 		  y1={yScale(yScale.domain()[0])}
 		  y2={yScale(d.y)}
   		stroke={'steelblue'}
   		strokeWidth={2} />
-  	<circle 
-			cx={xScale(d.x)}
-			cy={yScale(d.y)}
-			r={8}
-			fill={"green"}/>
+}
+
+function makeCircle(xScale, yScale, d){
+	return <circle 
+		cx={xScale(d.x)}
+		cy={yScale(d.y)}
+		r={8}
+		fill={"green"}/>
+}
+
+function makeLollipops(d,ind,xVal,yScale,xScale){
+	let thisLine = makeLine(xScale,yScale,d)
+	let thisCircle = makeCircle(xScale, yScale, d)
+	return(<g key={`${ind}${d[xVal]}`}>
+		{thisLine}
+		{thisCircle}
   </g>)
 }
 
