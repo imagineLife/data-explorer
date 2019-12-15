9359253e5ee5a544f1b0a695938ae836da133c71
Eric Laursen <ericjlaursen@gmail.com>
2019-12-15 15:14:28 -0500

    random-ish data generator

---
+++ data/dataGen.js
@@ -0,0 +1,42 @@
+let res = ``
+let howManyRows = 50;
+let st = new Date()
+function randomInt(min, max) { // min and max included 
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+// id,hosp,station,riskScore,outlierOccurances,daysOpen,lastActivity,meds
+let hospArr = ['HospA', 'HospB', 'HospC'];
+let stationArr = ['ER', 'HeartSpot', 'PACU'];
+let meds = ['heroin','cocaine','methamphetamine','dxm','mdma', 'Psilocybin']
+
+for(let i = 1; i <=howManyRows; i++){
+	console.log('i')
+	console.log(i)
+	
+	let thisID = randomInt(100,999)
+	let thisHops = hospArr[Math.floor(Math.random()*hospArr.length)]
+	let thisStation = stationArr[Math.floor(Math.random()*stationArr.length)]
+	let thisRisk = randomInt(75,99)
+	let occs = randomInt(4,17)
+	let days = randomInt(2,24)
+	let lastAct = randomInt(30,720)
+	let medCount = randomInt(1,6)
+	let thisMeds = []
+	for(let i = 1; i <=medCount; i++){
+		let rndMed = meds[Math.floor(Math.random()*meds.length)]
+		thisMeds = [...thisMeds, rndMed]
+	}
+
+	let resStr = `user-${thisID},${thisHops},${thisStation},${thisRisk},${occs},${days},${lastAct},${JSON.stringify(thisMeds)}\n`
+	res += resStr
+	console.log('// - - - - - //')
+}
+
+let end = new Date()
+let dur = end - st;
+console.log('dur')
+console.log(dur)
+
+console.log('res')
+console.log(res)
