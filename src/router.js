bdc7f0a516ee0c55aef51d0b39748fb91b8e1023
Eric Laursen <ericjlaursen@gmail.com>
2020-08-17 19:25:08 -0400

    new router

--- src/router.js
+++ src/router.js
@@ -3,12 +3,14 @@ import { render } from "react-dom"
 import { Router, Link } from "@reach/router"
 import Home from './Home'
 import Dash from './Dash'
+import MedaUploader from './MedaUploader'
 
 const ThisRouter = () => {
   return(
     <Router>
       <Home path="/" />
       <Dash path="dashboard" />
+      <MedaUploader path="meda" />
     </Router>
   )
 } 
